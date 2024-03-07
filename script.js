46ed4a6e65ffb61a9a9f4fc55924ffb4fc4be5ad
Soumencsit <140935846+Soumencsit@users.noreply.github.com>
2024-03-07 23:27:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// fetch("./country-by-population.json")
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+
+
+
+
+  url: 'https://get-population.p.rapidapi.com/population/country',
+
